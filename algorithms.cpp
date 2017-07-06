e8a335b648bba8ba2a0782e41e82834602cb331a
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2017-07-05 21:30:03 -0300

    Agora FIFO OK

--- algorithms.cpp
+++ algorithms.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <utility>
 #include <cstdlib>
+#include <queue>
 
 using namespace std;
 class LRU{
@@ -165,6 +166,54 @@ class Random{
 		}
 };
 
+class FIFO {
+    private:
+        vector<int> cache;
+        int misses_comp;
+        int misses_cap;
+        int hits;
+    public:
+        FIFO() : misses_comp(0), misses_cap(0), hits(0) {}
+        void exe(int s, vector<int> words) {
+            cout << "\nExecutando FIFO..." << endl;
+            vector<int>::iterator i;
+            vector<int>::iterator j;
+            queue<int> pos_q;
+            bool found;
+            for (i = words.begin(); i != words.end(); i++) {
+                cout << "Elemento: " << (*i);
+                found = false;
+                for (j = cache.begin(); j != cache.end(); j++) {
+                    if ((*j) == (*i)) {
+                        cout<<"\thit."<<endl;
+                        hits++;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    if (cache.size() < s) {
+                        misses_comp++;
+                        cache.push_back(*i);
+                        pos_q.push(cache.size() - 1);
+                        cout << "\tmiss\tElemento adicionado. " << (cache.size() - 1) << endl;
+                    } else {
+                        misses_cap++;
+                        cout << "\tmiss\tElemento trocado: " << *(cache.begin() + pos_q.front()) << '.' << endl;
+                        *(cache.begin() + pos_q.front()) = *i;
+                        pos_q.push(pos_q.front());
+                        pos_q.pop();
+                    }
+                }
+            }
+            cout<<"\nFinal da execucao. Resultados: "<<endl;
+            cout<<"-Hits: "<<hits<<endl;
+            cout<<"-Misses compulsorios: "<<misses_comp<<endl;
+            cout<<"-Misses de capacidade: "<<misses_cap<<endl;
+            hits = misses_comp = misses_cap = 0;
+        }
+ };
+
 int main(){
     int size;
     vector<int> words;
@@ -184,5 +233,8 @@ int main(){
     srand(time(NULL));
     Random random;
     random.exe(size, words);
+
+    FIFO fifo;
+    fifo.exe(size, words);
     
 }
